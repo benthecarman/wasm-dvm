c7095c8ab899c0d73ad280f4499da23c3d5e208c
benthecarman <benthecarman@live.com>
2024-01-29 15:29:34 +0000

    Timeout requests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::Cursor;
 use std::path::PathBuf;
+use std::time::Duration;
 
 use anyhow::anyhow;
 use clap::Parser;
@@ -10,6 +11,7 @@ use nostr::{Event, EventBuilder, EventId, Filter, Keys, Kind, Metadata, Tag, Tag
 use nostr_sdk::{Client, RelayPoolNotification};
 use reqwest::Url;
 use serde::{Deserialize, Serialize};
+use tokio::select;
 
 use crate::config::{Config, ServerKeys};
 
@@ -20,6 +22,7 @@ pub struct JobParams {
     pub url: String,
     pub function: String,
     pub input: String,
+    pub time: u64,
 }
 
 #[tokio::main]
@@ -173,6 +176,7 @@ pub async fn download_and_run_wasm(
     let temp_dir = tempfile::tempdir()?;
     let file_path = temp_dir.path().join(format!("{event_id}.wasm"));
 
+    // todo create reqwest client
     let response = reqwest::get(url).await?;
 
     if response.status().is_success() {
@@ -184,22 +188,39 @@ pub async fn download_and_run_wasm(
     };
 
     info!("Running wasm for event: {event_id}");
-    run_wasm(&file_path, job_params)
+    run_wasm(file_path, job_params).await
 }
 
-pub fn run_wasm(file_path: &PathBuf, job_params: JobParams) -> anyhow::Result<String> {
+pub async fn run_wasm(file_path: PathBuf, job_params: JobParams) -> anyhow::Result<String> {
     let wasm = Wasm::file(file_path);
-    let manifest = Manifest::new([wasm]);
+    let mut manifest = Manifest::new([wasm]);
+    manifest.allowed_hosts = Some(vec!["*".to_string()]);
     let mut plugin = Plugin::new(manifest, [], true)?;
-    let res = plugin.call::<&str, &str>(&job_params.function, &job_params.input)?;
+    let cancel_handle = plugin.cancel_handle();
+    let fut = tokio::task::spawn_blocking(move || {
+        plugin
+            .call::<&str, &str>(&job_params.function, &job_params.input)
+            .map(|x| x.to_string())
+    });
 
-    Ok(res.to_string())
+    let sleep = tokio::time::sleep(Duration::from_secs(job_params.time));
+
+    select! {
+        result = fut => {
+            result?
+        }
+        _ = sleep => {
+            cancel_handle.cancel()?;
+            Err(anyhow!("Timeout"))
+        }
+    }
 }
 
 #[cfg(test)]
 mod test {
     use crate::{download_and_run_wasm, JobParams};
     use nostr::EventId;
+    use serde_json::Value;
 
     #[tokio::test]
     async fn test_wasm_runner() {
@@ -208,6 +229,7 @@ mod test {
                 .to_string(),
             function: "count_vowels".to_string(),
             input: "Hello World".to_string(),
+            time: 5,
         };
         let result = download_and_run_wasm(params, EventId::all_zeros())
             .await
@@ -217,5 +239,37 @@ mod test {
             result,
             "{\"count\":3,\"total\":3,\"vowels\":\"aeiouAEIOU\"}"
         );
+    }
+
+    #[tokio::test]
+    async fn test_http_wasm() {
+        let params = JobParams {
+            url: "https://github.com/extism/plugins/releases/download/v0.5.0/http.wasm".to_string(),
+            function: "http_get".to_string(),
+            input: "{\"url\":\"https://benthecarman.com/.well-known/nostr.json\"}".to_string(), // get my nip05
+            time: 5,
+        };
+        let result = download_and_run_wasm(params, EventId::all_zeros())
+            .await
+            .unwrap();
+
+        let json = serde_json::from_str::<Value>(&result);
+
+        assert!(json.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_timeout_infinite_loop() {
+        let params = JobParams {
+            url: "https://github.com/extism/plugins/releases/download/v0.5.0/loop_forever.wasm"
+                .to_string(),
+            function: "loop_forever".to_string(),
+            input: "".to_string(),
+            time: 1,
+        };
+        let err = download_and_run_wasm(params, EventId::all_zeros()).await;
+
+        assert!(err.is_err());
+        assert_eq!(err.unwrap_err().to_string(), "Timeout");
     }
 }
